162c67222b6be0c8ba191cbbc320d208271f3d8f
Krishna <drkaojha@gmail.com>
2020-07-12 18:05:36 +0530

    basicUI setup

--- paypal/src/App.js
+++ paypal/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [checkout, setCheckout] = React.useState(false);
+  const [backButton, setBackButton] = React.useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React-PayPal</h1>
+        <button className="checkout-button">Checkout</button>
       </header>
     </div>
   );
 }
 
-export default App;
+
